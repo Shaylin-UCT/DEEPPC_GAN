71b8facc11b492e77fe4a081d1dacae5680592eb
Shaylin-UCT <chtsha042@myuct.ac.za>
2023-06-05 22:49:17 +0200

    Added info

---
+++ StyleGAN2.py
@@ -0,0 +1,6 @@
+'''
+Use stylegan2-ada-pytorch-main that originally comes from https://github.com/NVlabs/stylegan2-ada-pytorch
+1. Use dataset_tool to format data correctly
+2. Use train.py to train the model
+3. Figure out how to generate samples
+'''
